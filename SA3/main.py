9a6f39944c0f3aab03136bc7683d4307758d58c1
Vishal Gaddam <41083456+vishalgaddam873@users.noreply.github.com>
2023-11-21 18:28:46 +0530

    Update main.py

--- SA3/main.py
+++ SA3/main.py
@@ -1,31 +1,44 @@
+# -------------------
+# Install PyPDF2 library using below command
+# pip3 install 'PyPDF2<3.0'
+
+# Dictionary Attack
+# --------------------
+
 import PyPDF2 as pd
-filename = input('Path to the file: ')
-filename = filename.strip()
-file = open(filename,'rb')
-pdfReader = pd.PdfFileReader(file)
 
-tried = 0
 
-if not pdfReader.isEncrypted:
-    print('The file is not password protected! You can successfully open it!')
+def main():
+    # Get the target file path from the user
+    filename = input('Path to the file: ')
+    filename = filename.strip()
+    # Read the PDF file as binary object
+    file = open(filename, 'rb')
+    pdfReader = pd.PdfFileReader(file)
 
-else:
-    wordListFile = open('wordlist.txt', 'r',errors='ignore')
-    body = wordListFile.read().lower()
-    words = body.split('\n')
+    attempts = 0
 
-    for i in range(len(words)):
-        word = words[i]
-        print('Trying to decode passowrd by: {}'.format(word))
-        result = pdfReader.decrypt(word)
-        if result == 1:
-            print('Success! The password is: '+ word)
-            break
+    # Checks if the file is password encrypted
+    if not pdfReader.isEncrypted:
+        print('The file is not password protected! You can successfully open it!')
+    else:
+        # Reading the wordlist to perfrom Dictionary attack
+        wordListFile = open('wordlist.txt', 'r', errors='ignore')
+        body = wordListFile.read().lower()
+        words = body.split('\n')
 
-        elif result == 0:
-            tried += 1
-            print('Passwords tried: ' + str(tried))
-            continue
-            
+        for i in range(len(words)):
+            word = words[i]
+            print(f'Trying to decode passowrd by: {word}')
+            result = pdfReader.decrypt(word)
+            if result == 1:
+                print(f"Congratulations!!! The password is {word}.")
+                break
 
-#
+            elif result == 0:
+                attempts += 1
+                print(f'Passwords attempts: {str(attempts)}')
+                continue
+
+
+main()
